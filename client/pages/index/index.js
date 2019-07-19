f35cc1c0a96ff7e4472258937a4e8721870a178e
e <1213618287@qq.com>
2019-07-19 22:08:44 +0800

    第二次提交

--- client/pages/index/index.js
+++ client/pages/index/index.js
@@ -18,5 +18,15 @@ Page({
         console.log("跳转成功")
       },
     })
-  }
+  },
+  checkMovie() {
+    wx: wx.navigateTo({
+      url: '../movie/movie',
+      success: function (res) { },
+      fail: function (res) { },
+      complete: function (res) {
+        console.log("跳转成功")
+      },
+    })
+  },
 })
---
+++ client/pages/movie/movie.js
@@ -0,0 +1,3 @@
+// pages/newIndex/newIndex.js
+Page({
+})
